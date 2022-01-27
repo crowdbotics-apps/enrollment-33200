dead39d3f9a0ee6bb6625852777a198c0ea19eb1
Crowdbotics <team@crowdbotics.com>
2022-01-27 09:06:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/enrollment_33200/MainActivity.java
@@ -0,0 +1,15 @@
+package com.enrollment_33200;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "enrollment_33200";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "enrollment_33200",
+  url: "https://enrollment_33200.botics.co",
+  api: "https://enrollment_33200.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
